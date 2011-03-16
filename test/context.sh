a0283a42617efc9a3d2f73d92ad7f3cd7bb6b910
julp <julp@redaction-developpez.com>
2011-03-16 18:46:51 +0100

    too much complicated outputs?

--- test/context.sh
+++ test/context.sh
@@ -2,13 +2,15 @@
 
 # charset: UTF-8
 
-# WARNING: grep considers BOM when ugrep don't !
-# Comparaison must be about an UTF-8 file with LF line ending
+# WARNING:
+# * only file in UTF-8 *without BOM* should be compared (grep considers BOM when ugrep don't)
+# * comparaison must be about an UTF-8 file with LF line ending (grep considers CR when ugrep don't)
+# * search string should be ASCII only when LC_ALL=C
 
 . test/assert.sh.inc
 
 ARGS='--color=never -HnA 2 -B 3 after_context ugrep.c'
-assertOutputValue "-A 2 -B 3 (without -v)" "LC_ALL=C ./ugrep ${ARGS} 2>/dev/null" "grep ${ARGS}"
-assertOutputValue "-A 2 -B 3 (with -v)" "LC_ALL=C ./ugrep -v ${ARGS} 2>/dev/null" "grep -v ${ARGS}"
+assertOutputValueEx "-A 2 -B 3 (without -v)" "LC_ALL=C ./ugrep ${ARGS} 2>/dev/null" "grep ${ARGS}"
+assertOutputValueEx "-A 2 -B 3 (with -v)" "LC_ALL=C ./ugrep -v ${ARGS} 2>/dev/null" "grep -v ${ARGS}"
 
 exit $?
