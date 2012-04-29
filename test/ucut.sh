78ee96e05126989db6d6821e9dc3d3f002abd80d
julp <poinsot.julien@gmail.com>
2012-04-29 16:26:45 +0200

    ucut: add 2 UT

--- test/ucut.sh
+++ test/ucut.sh
@@ -26,6 +26,14 @@ INPUT="echo \"${E_ACUTE_NFD}l${RING_ABOVE}${E_GRAVE_NFD}v${RING_ABOVE}e${RING_AB
 ARGS='--unit=grapheme --form=d -c 4-'
 assertOutputValue "-c (graphemes)" "${INPUT} | ./ucut ${ARGS} 2>/dev/null" "v${RING_ABOVE}e${RING_ABOVE}"
 
+INPUT="echo \"${E_ACUTE_NFD}l${RING_ABOVE}${E_GRAVE_NFD}v${RING_ABOVE}e${RING_ABOVE}\""
+ARGS='--unit=grapheme --form=d -c 2-4'
+assertOutputValue "-c (graphemes)" "${INPUT} | ./ucut ${ARGS} 2>/dev/null" "l${RING_ABOVE}${E_GRAVE_NFD}v${RING_ABOVE}"
+
+INPUT="echo \"1:2:3:4:5:6:7:8:9\""
+ARGS='-d: -f 3-6'
+assertOutputValue "-f (fields)" "${INPUT} | ./ucut ${ARGS} 2>/dev/null" "3:4:5:6"
+
 # Disabled: need to fix -E separator handling first
 # INPUT='echo "0,1,2,3,4,5,6,7,8,9"'
 # ARGS='-d , -f 1-3,6-8'
